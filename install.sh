b25ca52f9c70fdf69ec9a1677f40ba283e3589d5
Julian Pr <Julian.Proest@edu.fhdw.de>
2026-06-12 13:32:47 +0200

    change6 install

--- install.sh
+++ install.sh
@@ -3,20 +3,6 @@ Failcounter=0
 #Verzeichnisstruktur erstellen
 
 while [ "$Failcounter" -lt "5" ]; do
-	mkdir -p "./netpulse"
-		if [ $? -ne "0" ]; then
-		((Failcounter++)) 
-		else echo "successfully created directory netpulse"
-		Failcounter=0
-		break
-		fi
-done
-if [ "$Failcounter" -eq "5" ]; then
-	echo "failed to create directory netpulse"
-	exit 1
-fi
-
-while [ "$Failcounter" -lt "5" ]; do
 	sudo apt install python3-venv -y
 		if [ $? -ne "0" ]; then
 		((Failcounter++)) 
@@ -27,37 +13,6 @@ while [ "$Failcounter" -lt "5" ]; do
 done
 if [ "$Failcounter" -eq "5" ]; then
 	echo "failed to install python3"
-	exit 1
-fi
-
-cd netpulse
-python3 -m venv .venv
-sudo apt install python3-pip -y
-while [ "$Failcounter" -lt "5" ]; do
-	cp main.py "./netpulse"
-		if [ $? -ne "0" ]; then
-		((Failcounter++)) 
-		else echo "successfully copied main.py"
-		Failcounter=0 
-		break
-		fi
-done
-if [ "$Failcounter" -eq "5" ]; then
-	echo "failed to copy main.py"
-	exit 1
-fi
-
-while [ "$Failcounter" -lt "5" ]; do
-	cp monitoring "./netpulse"
-		if [  $? -ne "0" ]; then
-		((Failcounter++))
-		else echo "successfully copied monitoring folder"
-		Failcounter=0 
-		break
-		fi
-done
-if [ "$Failcounter" -eq "5" ]; then
-	echo "failed to copy the monitoring folder"
 	exit 1
 fi
 
@@ -78,12 +33,7 @@ cd ..
 cd monitoring
 docker compose up -d
 cd ..
-cd netpulse
 fastapi dev main.py
 
 
 exit
-
-
-
-
